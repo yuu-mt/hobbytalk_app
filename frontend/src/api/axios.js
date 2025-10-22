8480cac760e8a7163ee28af352c495bd431af2c8
yuu-mt <yuu.is3mt@gmail.com>
2025-10-22 13:24:24 +0900

    新規登録データベース連携

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 axios.defaults.withCredentials = true; // Cookie送信を許可
-axios.defaults.baseURL = "http://localhost:8080"; // Laravel APIのURL
+axios.defaults.baseURL = "http://localhost:8080/api"; // Laravel APIのURL
+const api = axios.create({
+  baseURL:"http://localhost:8080/api",
+  withCredentials:true,
+});
 
-export default axios;
+export default api;
